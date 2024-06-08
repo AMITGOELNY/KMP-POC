ad9eff972e779f740c91be581c6e1cd40d23ed3f
amit goel <amit.goel@wildhealth.com>
2024-06-08 19:00:43 -0400

    Configure basic API client

---
+++ shared/src/commonMain/kotlin/com/amit/kmp/poc/shared/data/api/ApiClient.kt
@@ -0,0 +1,66 @@
+package com.amit.kmp.poc.shared.data.api
+
+import io.ktor.client.HttpClient
+import io.ktor.client.plugins.addDefaultResponseValidation
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.defaultRequest
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.client.plugins.logging.SIMPLE
+import io.ktor.client.request.HttpRequestPipeline
+import io.ktor.http.ContentType
+import io.ktor.http.URLProtocol
+import io.ktor.http.contentType
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+import org.koin.core.annotation.Single
+
+private const val BASE_URL = "138.197.84.104"
+
+@Single([ApiClient::class])
+internal class ApiClient(
+    httpClient: HttpClient,
+//    private val preferenceManager: PreferenceManager
+) {
+    val http =
+        httpClient.config {
+            install(Logging) {
+                logger = Logger.SIMPLE
+                level = LogLevel.BODY
+            }
+
+            install(ContentNegotiation) {
+                json(
+                    Json {
+                        ignoreUnknownKeys = true
+                        encodeDefaults = true
+                        isLenient = true
+                    },
+                )
+            }
+            defaultRequest {
+                contentType(ContentType.Application.Json)
+                url {
+                    protocol = URLProtocol.HTTP
+                    host = BASE_URL
+                }
+            }
+            expectSuccess = true
+            addDefaultResponseValidation()
+
+            install("TokenHandler") {
+                requestPipeline.intercept(HttpRequestPipeline.Before) {
+//                if (context.url.pathSegments.first() != "login") {
+//                    preferenceManager.get<String>(Preferences.USER_TOKEN_KEY)?.let {
+//                        context.header("Authorization", "Bearer $it")
+//                    }
+//                }
+                }
+            }
+        }
+
+//    fun storeToken(token: String) {
+//        preferenceManager[Preferences.USER_TOKEN_KEY] = token
+//    }
+}
