30995b82ac6dcf35f678b99658f70bad6d301a6b
amit goel <amit.goel@wildhealth.com>
2024-06-11 23:26:56 -0400

    Update ios UI

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -8,38 +8,84 @@ struct ContentView: View {
     @StateObject var viewModelStoreOwner = SharedViewModelStoreOwner<FeedViewModel>()
     
     var body: some View {
-        VStack {
             NavigationView {
-                VStack(spacing: 0) {
+                Group {
                     Observing(viewModelStoreOwner.instance.state) { state in
                         switch onEnum(of: state.feed) {
                         case .loading:
-                            Text("loading")
+                            ProgressView()
+                                .padding()
                         case .empty:
-                            Text("empty")
+                            Text("No News")
+                                .font(.headline)
+                                .padding()
                         case .error:
-                            Text("error")
+                            Text("Error")
+                                .font(.headline)
+                                .padding()
                         case .loaded(let success):
-                            List(success.data?.items ?? []) { item in
-                                Text(item.title)
-                            }
-                            .listStyle(.plain)
-                            .navigationBarTitle(Text("Trending News"))
-                            .onAppear {
-                                UITableView.appearance().separatorStyle = .none
-                            }
+                            newsFeedList(feedItems: success.data?.items ?? [])
                         }
                     }
                 }
-                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
-                .padding()
+                .navigationTitle("Trending News")
+                .frame(maxWidth: .infinity)
             }
-        }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+@ViewBuilder
+func newsFeedList(feedItems: [FeedItem]) -> some View {
+    
+    ScrollView {
+        VStack(spacing: 12) {
+            ForEach(feedItems) { feedItem in
+                HStack(spacing: 10) {
+                    
+                    AsyncImage(url: URL(string: feedItem.image)) { image in
+                        image
+                            .resizable()
+                    } placeholder: {
+                        ProgressView()
+                    }
+                    .frame(width: 100, height: 100)
+                    .clipShape(
+                        .rect(
+                            topLeadingRadius: 12,
+                            bottomLeadingRadius: 12,
+                            bottomTrailingRadius: 0,
+                            topTrailingRadius: 0
+                        )
+                    )
+                    
+                    VStack(alignment: .leading, spacing: 5) {
+                        Text(feedItem.title)
+                            .font(.headline)
+                            .lineLimit(2)
+                            .padding(.top, 6)
+                        
+                        Spacer()
+                        
+                        Text(feedItem.site)
+                            .font(.caption)
+                            .foregroundStyle(Color.orange)
+                            .padding(.bottom, 6)
+                    }
+                    
+                    Spacer()
+                    
+                }
+                .frame(maxWidth: .infinity)
+                .frame(height: 100)
+                .clipShape(RoundedRectangle(cornerRadius: 12, style: .continuous))
+                .overlay(
+                    RoundedRectangle(cornerRadius: 12)
+                        .stroke(Color.gray, lineWidth: 0.8)
+                )
+                .padding(.vertical, 2)
+            }
+        }
+        .padding()
     }
+
 }
